f72b3bbc743523b4a1c21522423178e35820b3b2
akshatvishnoi99 <akshatvishnoi99@gmail.com>
2025-04-23 17:38:39 +0530

    nav bar modified

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,34 @@
+import "./Navbar.css";
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const a = 2;
+  console.log(a);
   return (
-    <nav >
-      <Link to="/" >Home</Link>
-      <Link to="/shop" >Shop</Link>
-      <Link to="/about" >About</Link>
-      <Link to="/cart" >Cart</Link>
-      <Link to="/login" >Login</Link>
-    </nav>
+    < >
+      <nav className="flexbox">
+        <div id="logo">
+          <div>
+            <link href="https://fonts.googleapis.com/css2?family=Irish+Grover&display=swap" rel="stylesheet"></link>
+            OffStore
+          </div>
+          <div>
+            wear the vibe
+          </div>
+        </div>
+        <div id="search" className="flexbox">
+          <input type="text" placeholder="Find Your Styles"></input>
+        </div>
+        <ul className="flexbox">
+          <li><Link to="/" className="link">Home</Link></li>
+          <li><Link to="/shop" className="link">Shop</Link></li>
+          <li><Link to="/about" className="link">About</Link></li>
+          <li><Link to="/cart" className="link">Cart</Link></li>
+          <li><Link to="/login" className="link">Login</Link></li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
