5a79a675fd8ec3b87f9e46912beca5cade589489
Kshitij Kumar Srivastavae <kshitijsrivastava505@gmail.comm>
2025-04-23 19:28:28 +0530

    hhh

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,2 +1,19 @@
-const Home = () => <h1></h1>;
+import './Home.css'
+const Home =() => {
+
+    return(
+        <>
+        <div className="home1">
+            <div className="home1-img1">
+            <img src="src\assets\9706877.jpg" ></img>
+            </div>
+            <div className="home1-img2">
+            <img src="src\assets\handsome-tourist-straw-hat-put-sunglasses-summer-vacation-standing-blue-background.jpg" ></img>
+            </div>
+        </div>
+
+        </>
+
+ )
+}
 export default Home;
--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -1,2 +1,3 @@
 const Shop = () => <h1>Welcome to Shop Page</h1>;
+
 export default Shop;
