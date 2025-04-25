6a50a17c3a03ba7b943457c3c6eb03cb14a3f147
akshatvishnoi99 <akshatvishnoi99@gmail.com>
2025-04-26 01:24:40 +0530

    hhh

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -1,9 +1,34 @@
+import './Shop.css'
 const Shop = () => {
    return(
      <>
-       <h1>
-        Welcome to Shop Page
-       </h1>
+       <div className="home1">
+        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="ride">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src="src\assets\9706877.jpg" className="d-block w-100" alt="..."></img>
+            </div>
+            <div className="carousel-item">
+              <img src="src\assets\9701606.jpg" className="d-block w-100" alt="..."></img>
+            </div>
+            <div className="carousel-item">
+              <img src="src\assets\handsome-tourist-straw-hat-put-sunglasses-summer-vacation-standing-blue-background.jpg" className="d-block w-100" alt="..."></img>
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+      <div className='Line flexbox'>
+      <div className='flexbox'><h1>MENS FASHION</h1><button className='dropdown'></button></div>
+      <div className='flexbox'><h1>WOMENS FASHION</h1><button className='dropdown'></button></div>
+      </div>
      </>
     );
 };
