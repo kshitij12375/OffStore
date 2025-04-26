f1d6e0f904e8b11261ae2c73aceba7292b7a40ea
Kshitij Kumar Srivastavae <kshitijsrivastava505@gmail.comm>
2025-04-26 21:39:09 +0530

    hhhh

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,12 +8,21 @@ const Home =() => {
   <div className="carousel-inner">
     <div className="carousel-item active">
       <img src="src\assets\9706877.jpg" className="d-block w-100" alt="..."></img>
+      <h1 className='text3'>"Your favorite products, just a click away."</h1>
     </div>
     <div className="carousel-item">
       <img src="src\assets\9701606.jpg" className="d-block w-100" alt="..."></img>
+      <h1 className='text2'>"Shopping made simple. Style made personal."
+
+</h1>
+      
     </div>
+    
     <div className="carousel-item">
       <img src="src\assets\handsome-tourist-straw-hat-put-sunglasses-summer-vacation-standing-blue-background.jpg" className="d-block w-100" alt="..."></img>
+      <h1 className='text1'>"Where trends begin and savings never end."
+
+</h1>
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
@@ -26,34 +35,163 @@ const Home =() => {
   </button>
 </div>
         </div>
+       
         <div className='Line'><h1>New Collections</h1></div>
         <div className="home2">
             <div className='card1'>
                 <div className='card2'>
                     <img src="src\assets\shirt4.webp" alt="shirt4"></img>
+                    <div className='card3'>
+                    <p id='h'>Mast and Habour</p>
+                    <p>Pure Cotton and tailored Shirt</p>
+                    <p>Rs. 999</p>
+                    </div>
                     </div>
             </div>
             <div className='card1'>
             <div className='card2'>
                     <img src="src\assets\shirt4.webp" alt="shirt4"></img>
+                    <div className='card3'>
+                    <p id='h'>Mast and Habour</p>
+                    <p>Pure Cotton and tailored Shirt</p>
+                    <p>Rs. 999</p>
+                    </div>
                     </div>
             </div>
             <div className='card1'>
             <div className='card2'>
                     <img src="src\assets\shirt4.webp" alt="shirt4"></img>
+                    <div className='card3'>
+                    <p id='h'>Mast and Habour</p>
+                    <p>Pure Cotton and tailored Shirt</p>
+                    <p>Rs. 999</p>
+                    </div>
+                    
                     </div>
             </div>
             <div className='card1'>
             <div className='card2'>
                     <img src="src\assets\shirt4.webp" alt="shirt4"></img>
+                    <div className='card3'>
+                    <p id='h'>Mast and Habour</p>
+                    <p>Pure Cotton and tailored Shirt</p>
+                    <p>Rs. 999</p>
+                    </div>
+
                     </div>
             </div>
             <div className='card1'>
             <div className='card2'>
                     <img src="src\assets\shirt4.webp" alt="shirt4"></img>
+                    <div className='card3'>
+                    <p id='h'>Mast and Habour</p>
+                    <p>Pure Cotton and tailored Shirt</p>
+                    <p>Rs. 999</p>
+                    </div>
+                    </div>
+            </div>
+            <div className='card1'>
+            <div className='card2'>
+                    <img src="src\assets\shirt4.webp" alt="shirt4"></img>
+                    <div className='card3'>
+                    <p id='h'>Mast and Habour</p>
+                    <p>Pure Cotton and tailored Shirt</p>
+                    <p>Rs. 999</p>
+                    </div>
                     </div>
             </div>
             </div>
+            <div className="home2">
+            <div className='card1'>
+                <div className='card2'>
+                    <img src="src\assets\shirt4.webp" alt="shirt4"></img>
+                    <div className='card3'>
+                    <p id='h'>Mast and Habour</p>
+                    <p>Pure Cotton and tailored Shirt</p>
+                    <p>Rs. 999</p>
+                    </div>
+                    </div>
+            </div>
+            <div className='card1'>
+            <div className='card2'>
+                    <img src="src\assets\shirt4.webp" alt="shirt4"></img>
+                    <div className='card3'>
+                    <p id='h'>Mast and Habour</p>
+                    <p>Pure Cotton and tailored Shirt</p>
+                    <p>Rs. 999</p>
+                    </div>
+                    </div>
+            </div>
+            <div className='card1'>
+            <div className='card2'>
+                    <img src="src\assets\shirt4.webp" alt="shirt4"></img>
+                    <div className='card3'>
+                    <p id='h'>Mast and Habour</p>
+                    <p>Pure Cotton and tailored Shirt</p>
+                    <p>Rs. 999</p>
+                    </div>
+                    
+                    </div>
+            </div>
+            <div className='card1'>
+            <div className='card2'>
+                    <img src="src\assets\shirt4.webp" alt="shirt4"></img>
+                    <div className='card3'>
+                    <p id='h'>Mast and Habour</p>
+                    <p>Pure Cotton and tailored Shirt</p>
+                    <p>Rs. 999</p>
+                    </div>
+
+                    </div>
+            </div>
+            <div className='card1'>
+            <div className='card2'>
+                    <img src="src\assets\shirt4.webp" alt="shirt4"></img>
+                    <div className='card3'>
+                    <p id='h'>Mast and Habour</p>
+                    <p>Pure Cotton and tailored Shirt</p>
+                    <p>Rs. 999</p>
+                    </div>
+                    </div>
+            </div>
+            <div className='card1'>
+            <div className='card2'>
+                    <img src="src\assets\shirt4.webp" alt="shirt4"></img>
+                    <div className='card3'>
+                    <p id='h'>Mast and Habour</p>
+                    <p>Pure Cotton and tailored Shirt</p>
+                    <p>Rs. 999</p>
+                    </div>
+                    </div>
+            </div>
+            </div>
+            <div className='home3'></div>
+            <div className="footer">
+              <div className='footer1'>
+              <div id="logo">
+          <div>
+            <link href="https://fonts.googleapis.com/css2?family=Irish+Grover&display=swap" rel="stylesheet"></link>
+            OffStore
+         
+          </div>
+         
+          
+         
+        </div>
+        <div className='text4'>
+          <h4>Contact</h4>
+          <p>+91 1234567890</p>
+          <p> Adress:New York</p>
+          <p>Gmail:abc@gmail.com</p>
+          
+
+        
+        </div>
+        </div>
+                <div className='footer2'>
+                </div>
+            </div>
+
 
         </>
 
