c2d6bb729c203a98f8fcabb55a5a634a3cdb500d
akshatvishnoi99 <akshatvishnoi99@gmail.com>
2025-04-29 03:12:00 +0530

    shop page updated and assets added

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -1,4 +1,10 @@
-import './Shop.css'
+import './Shop.css';
+import tshirt1 from '../assets/tshirt1.jpg';
+import tshirt3 from '../assets/tshirt3.jpg';
+import tshirt4 from '../assets/tshirt4.webp';
+import tshirt5 from '../assets/tshirt5.webp';
+import rating from '../assets/rating.png';
+import cart from '../assets/cart.png';
 const Shop = () => {
    return(
      <>
@@ -26,45 +32,160 @@ const Shop = () => {
         </div>
       </div>
       <div className='Line flexbox'>
-      <div className='flexbox'><h1>MENS FASHION</h1><button className='dropdown'></button></div>
-      <div className='flexbox'><h1>WOMENS FASHION</h1><button className='dropdown'></button></div>
+      <div className='flexbox'><h1>MENS FASHION</h1><button class="btn btn-secondary dropdown-toggle dropdown no-caret" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+      <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Shirts</a></li>
+    <li><a class="dropdown-item" href="#">TShirts</a></li>
+    <li><a class="dropdown-item" href="#">Trousers</a></li>
+    <li><a class="dropdown-item" href="#">Jeans</a></li>
+  </ul>
+  </button></div>
+      <div className='flexbox'><h1>WOMENS FASHION</h1><button class="btn btn-secondary dropdown-toggle dropdown no-caret" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+      <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Tshirts</a></li>
+    <li><a class="dropdown-item" href="#">Salwaar suits</a></li>
+    <li><a class="dropdown-item" href="#">Jeans</a></li>
+    <li><a class="dropdown-item" href="#">Traditionals</a></li>
+  </ul>
+  </button></div>
       </div>
       <div className="flexbox">
       <div id="MensFashion" className="grid">
-        <div className="card" id='cm1'></div>
-        <div className="card " id='cm2'></div>
-        <div className="details"></div>
-        <div className="details"></div>
-        <div className="card "></div>
-        <div className="card "></div>
-        <div className="details"></div>
-        <div className="details"></div>
-        <div className="card "></div>
-        <div className="card "></div>
-        <div className="details"></div>
-        <div className="details"></div>
-        <div className="card "></div>
-        <div className="card "></div>
-        <div className="details"></div>
-        <div className="details"></div>
+        <div className="card flexbox" id='cm1'><img src={tshirt1} alt="" /></div>
+        <div className="card flexbox" id='cm2'><img src={tshirt3} alt="" /></div>
+        <div className="details">
+          <div>Cream Embroidered Polo Slim Fit Shirt</div>
+          <div className='gridd'>
+            <div>Rs.450</div>
+            <div >Add to cart<img src={cart} alt="" />
+            </div>
+            <div >4.7<img src={rating} alt="" /></div>
+          </div>
+        </div>
+        <div className="details">
+        <div>Cream Embroidered Polo Slim Fit Shirt</div>
+          <div className='gridd'>
+            <div>Rs.450</div>
+            <div >Add to cart<img src={cart} alt="" /></div>
+            <div >4.7<img src={rating} alt="" /></div>
+          </div>
+        </div>
+        <div className="card flexbox"><img src={tshirt1} alt="" /></div>
+        <div className="card flexbox"><img src={tshirt3} alt="" /></div>
+        <div className="details"><div>Cream Embroidered Polo Slim Fit Shirt</div>
+          <div className='gridd'>
+            <div>Rs.450</div>
+            <div >Add to cart<img src={cart} alt="" /></div>
+            <div >4.7<img src={rating} alt="" /></div>
+          </div></div>
+        <div className="details"><div>Cream Embroidered Polo Slim Fit Shirt</div>
+          <div className='gridd'>
+            <div>Rs.450</div>
+            <div >Add to cart<img src={cart} alt="" /></div>
+            <div >4.7<img src={rating} alt="" /></div>
+          </div></div>
+        <div className="card flexbox"><img src={tshirt1} alt="" /></div>
+        <div className="card flexbox"><img src={tshirt3} alt="" /></div>
+        <div className="details"><div>Cream Embroidered Polo Slim Fit Shirt</div>
+          <div className='gridd'>
+            <div>Rs.450</div>
+            <div >Add to cart<img src={cart} alt="" /></div>
+            <div >4.7<img src={rating} alt="" /></div>
+          </div></div>
+        <div className="details"><div>Cream Embroidered Polo Slim Fit Shirt</div>
+          <div className='gridd'>
+            <div>Rs.450</div>
+            <div >Add to cart<img src={cart} alt="" /></div>
+            <div >4.7<img src={rating} alt="" /></div>
+          </div></div>
+        <div className="card flexbox"><img src={tshirt1} alt="" /></div>
+        <div className="card flexbox"><img src={tshirt3} alt="" /></div>
+        <div className="details"><div>Cream Embroidered Polo Slim Fit Shirt</div>
+          <div className='gridd'>
+            <div>Rs.450</div>
+            <div >Add to cart<img src={cart} alt="" /></div>
+            <div >4.7<img src={rating} alt="" /></div>
+          </div></div>
+        <div className="details"><div>Cream Embroidered Polo Slim Fit Shirt</div>
+          <div className='gridd'>
+            <div>Rs.450</div>
+            <div >Add to cart<img src={cart} alt="" /></div>
+            <div >4.7<img src={rating} alt="" /></div>
+          </div></div>
       </div>
       <div id="womensFashion" className="grid">
-        <div className="card "></div>
-        <div className="card "></div>
-        <div className="details"></div>
-        <div className="details"></div>
-        <div className="card "></div>
-        <div className="card "></div>
-        <div className="details"></div>
-        <div className="details"></div>
-        <div className="card "></div>
-        <div className="card "></div>
-        <div className="details"></div>
-        <div className="details"></div>
-       <div className="card "></div>
-        <div className="card "></div>
-        <div className="details"></div>
-        <div className="details"></div>
+        <div className="card flexbox"><img src={""}alt="" /></div>
+        <div className="card flexbox"><img src={""} alt="" /></div>
+        <div className="details">
+          <div>Mast and Harbour Striped Top</div>
+          <div className='gridd'>
+            <div>Rs.450</div>
+            <div >Add to cart<img src={cart} alt="" /></div>
+            <div >4.7<img src={rating} alt="" /></div>
+          </div>
+        </div>
+        <div className="details">
+        <div>Mast and Harbour Striped Top</div>
+          <div className='gridd'>
+            <div>Rs.450</div>
+            <div >Add to cart<img src={cart} alt="" /></div>
+            <div >4.7<img src={rating} alt="" /></div>
+          </div>
+        </div>
+        <div className="card flexbox"><img src={""}alt="" /></div>
+        <div className="card flexbox"><img src={""}alt="" /></div>
+        <div className="details">
+        <div>Mast and Harbour Striped Top</div>
+          <div className='gridd'>
+            <div>Rs.450</div>
+            <div >Add to cart<img src={cart} alt="" /></div>
+            <div >4.7<img src={rating} alt="" /></div>
+          </div>
+        </div>
+        <div className="details">
+        <div>Mast and Harbour Striped Top</div>
+          <div className='gridd'>
+            <div>Rs.450</div>
+            <div >Add to cart<img src={cart} alt="" /></div>
+            <div >4.7<img src={rating} alt="" /></div>
+          </div>
+        </div>
+        <div className="card flexbox"><img src={""}alt="" /></div>
+        <div className="card flexbox"><img src={""}alt="" /></div>
+        <div className="details">
+        <div>Mast and Harbour Striped Top</div>
+          <div className='gridd'>
+            <div>Rs.450</div>
+            <div >Add to cart<img src={cart} alt="" /></div>
+            <div >4.7<img src={rating} alt="" /></div>
+          </div>
+        </div>
+        <div className="details">
+        <div>Mast and Harbour Striped Top</div>
+          <div className='gridd'>
+            <div>Rs.450</div>
+            <div >Add to cart<img src={cart} alt="" /></div>
+            <div >4.7<img src={rating} alt="" /></div>
+          </div>
+        </div>
+       <div className="card flexbox"><img src={""}alt="" /></div>
+        <div className="card flexbox"><img src={""}alt="" /></div>
+        <div className="details">
+        <div>Mast and Harbour Striped Top</div>
+          <div className='gridd'>
+            <div>Rs.450</div>
+            <div >Add to cart<img src={cart} alt="" /></div>
+            <div >4.7<img src={rating} alt="" /></div>
+          </div>
+        </div>
+        <div className="details">
+        <div>Mast and Harbour Striped Top</div>
+          <div className='gridd'>
+            <div>Rs.450</div>
+            <div >Add to cart<img src={cart} alt="" /></div>
+            <div >4.7<img src={rating} alt="" /></div>
+          </div>
+        </div>
       </div>
       </div>
      </>
