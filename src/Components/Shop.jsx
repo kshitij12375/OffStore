9547d629f245b334adeec1b6a3fa390dbcec8a34
akshatvishnoi99 <akshatvishnoi99@gmail.com>
2025-05-09 15:07:20 +0530

    shop updated

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -25,8 +25,10 @@ const Shop = () => {
     const imgElement = document.getElementById(value)?.querySelector("img");
     const src = imgElement?.getAttribute("src") || "";
     const description = document.getElementById(clickedBtn.getAttribute("data-text"))?.innerText || "";
-    const amount = document.getElementById(clickedBtn.getAttribute("data-amount"))?.innerText || "";
-
+    const str = document.getElementById(clickedBtn.getAttribute("data-amount"))?.innerText || "";
+    const number = str.match(/\d+/g); // returns an array of matched numbers
+    const result = number ? number.join("") : "";
+    const amount = parseInt(result);
     const dataToSend = {
       image: src,
       description: description,
@@ -84,138 +86,139 @@ const Shop = () => {
         <div className="card flexbox" id='cm1'><img src="https://images.unsplash.com/photo-1611312449408-fcece27cdbb7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bWVucyUyMGNsb3RoZXN8ZW58MHx8MHx8fDA%3D" alt="" /></div>
         <div className="card flexbox" id='cm2'><img src="https://images.unsplash.com/photo-1611312449412-6cefac5dc3e4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG1lbnMlMjBjbG90aGVzfGVufDB8fDB8fHww" alt="" /></div>
         <div className="details">
-          <div id='t1'>Cream Embroidered Polo Slim Fit Shirt</div>
+          <div id='tm1'>Men's Casual Denim Shirt</div>
           <div className='gridd'>
-            <div id='a1'>Rs.450</div>
-            <div ><button className='cartbtn' onClick={btnclick} value='cm1' data-text="t1" data-amount="a1">Add to cart</button>
+            <div id='am1'>Rs.700</div>
+            <div ><button className='cartbtn' onClick={btnclick} value='cm1' data-text="tm1" data-amount="am1">Add to cart</button>
             </div>
             <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
           </div>
         </div>
         <div className="details">
-        <div id='t2'>Cream Embroidered Polo Slim Fit Shirt</div>
+        <div id='tm2'>Men's Casual Check Shirt</div>
           <div className='gridd'>
-           <div id="a2">Rs.460</div>
-            <div ><button className='cartbtn' onClick={btnclick} value='cm2' data-text="t2" data-amount="a2">Add to cart</button></div>
+           <div id="am2">Rs.460</div>
+            <div ><button className='cartbtn' onClick={btnclick} value='cm2' data-text="tm2" data-amount="am2">Add to cart</button></div>
             <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
           </div>
         </div>
-        <div className="card flexbox"><img src="https://images.unsplash.com/photo-1521498542256-5aeb47ba2b36?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8OTQ2MzI2NHx8ZW58MHx8fHx8" alt="" /></div>
-        <div className="card flexbox"><img src="https://images.unsplash.com/photo-1571455786673-9d9d6c194f90?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8ZU11M2RUa3MtQk18fGVufDB8fHx8fA%3D%3D" alt="" /></div>
-        <div className="details"><div>Cream Embroidered Polo Slim Fit Shirt</div>
-          <div className='gridd'>
-            <div>Rs.460</div>
-            <div >Add to cart</div>
-            <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
-          </div></div>
-        <div className="details"><div>Cream Embroidered Polo Slim Fit Shirt</div>
-          <div className='gridd'>
-            <div>Rs.450</div>
-            <div >Add to cart</div>
-            <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
-          </div></div>
-        <div className="card flexbox"><img src="https://media.istockphoto.com/id/182422958/photo/plain-white-t-shirt.webp?a=1&b=1&s=612x612&w=0&k=20&c=1-XoGdX1DxIEo_4GI7CVAOuoRd1EJBA4CKQ1dt68Ccg=" alt="" /></div>
-        <div className="card flexbox"><img src="https://images.unsplash.com/photo-1576566588028-4147f3842f27?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8ODQwMDgzODl8fGVufDB8fHx8fA%3D%3D" alt="" /></div>
-        <div className="details"><div>Cream Embroidered Polo Slim Fit Shirt</div>
-          <div className='gridd'>
-            <div>Rs.450</div>
-            <div >Add to cart</div>
-            <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
-          </div></div>
-        <div className="details"><div>Cream Embroidered Polo Slim Fit Shirt</div>
-          <div className='gridd'>
-            <div>Rs.450</div>
-            <div >Add to cart</div>
-            <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
-          </div></div>
-        <div className="card flexbox"><img src="https://images.unsplash.com/photo-1490168105446-f43395eb50b5?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8MTcyMjY1MHx8ZW58MHx8fHx8" alt="" /></div>
-        <div className="card flexbox"><img src="https://images.unsplash.com/photo-1464278860589-b2ed64f87e22?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8NDI4NzA2fHxlbnwwfHx8fHw%3D" alt="" /></div>
-        <div className="details"><div>Cream Embroidered Polo Slim Fit Shirt</div>
-          <div className='gridd'>
-            <div>Rs.450</div>
-            <div >Add to cart</div>
-            <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
-          </div></div>
-        <div className="details"><div>Cream Embroidered Polo Slim Fit Shirt</div>
-          <div className='gridd'>
-            <div>Rs.450</div>
-            <div >Add to cart</div>
-            <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
-          </div></div>
-          <div className="card flexbox" id='cm1'><img src="https://images.unsplash.com/photo-1512755051947-dea0029e93ad?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8MTE1MTA0MTh8fGVufDB8fHx8fA%3D%3D" alt="" /></div>
-        <div className="card flexbox" id='cm2'><img src="https://images.unsplash.com/photo-1527016021513-b09758b777bd?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8OTM2MTIxMXx8ZW58MHx8fHx8" alt="" /></div>
+        <div className="card flexbox" id='cm3'><img src="https://images.unsplash.com/photo-1521498542256-5aeb47ba2b36?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8OTQ2MzI2NHx8ZW58MHx8fHx8" alt="" /></div>
+        <div className="card flexbox" id='cm4'><img src="https://images.unsplash.com/photo-1571455786673-9d9d6c194f90?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8ZU11M2RUa3MtQk18fGVufDB8fHx8fA%3D%3D" alt="" /></div>
         <div className="details">
-          <div>Cream Embroidered Polo Slim Fit Shirt</div>
+          <div id='tm3'>Round Neck Slimfit Tshirt</div>
           <div className='gridd'>
-            <div>Rs.450</div>
-            <div >Add to cart
+            <div id='am3'>Rs.460</div>
+            <div ><button className='cartbtn' onClick={btnclick} value='cm3' data-text="tm3" data-amount="am3">Add to cart</button></div>
+            <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
+          </div></div>
+        <div className="details" id='tm4'><div>Round Neck Slimfit Tshirt</div>
+          <div className='gridd'>
+            <div id='am4'>Rs.450</div>
+            <div ><button className='cartbtn' onClick={btnclick} value='cm4' data-text="tm4" data-amount="am4">Add to cart</button></div>
+            <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
+          </div></div>
+        <div className="card flexbox" id='cm5'><img src="https://media.istockphoto.com/id/182422958/photo/plain-white-t-shirt.webp?a=1&b=1&s=612x612&w=0&k=20&c=1-XoGdX1DxIEo_4GI7CVAOuoRd1EJBA4CKQ1dt68Ccg=" alt="" /></div>
+        <div className="card flexbox" id='cm6'><img src="https://images.unsplash.com/photo-1576566588028-4147f3842f27?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8ODQwMDgzODl8fGVufDB8fHx8fA%3D%3D" alt="" /></div>
+        <div className="details" id='tm5'><div>Solid White Round Neck Tshirt</div>
+          <div className='gridd'>
+            <div id='am5'>Rs.350</div>
+            <div ><button className='cartbtn' onClick={btnclick} value='cm5' data-text="tm5" data-amount="am5">Add to cart</button></div>
+            <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
+          </div></div>
+        <div className="details" id='tm6'><div>Men's Fashion Trendy Tshirt</div>
+          <div className='gridd'>
+            <div id='am6'>Rs.650</div>
+            <div ><button className='cartbtn' onClick={btnclick} value='cm6' data-text="tm6" data-amount="am6">Add to cart</button></div>
+            <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
+          </div></div>
+        <div className="card flexbox" id='cm7'><img src="https://images.unsplash.com/photo-1490168105446-f43395eb50b5?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8MTcyMjY1MHx8ZW58MHx8fHx8" alt="" /></div>
+        <div className="card flexbox" id='cm8'><img src="https://images.unsplash.com/photo-1464278860589-b2ed64f87e22?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8NDI4NzA2fHxlbnwwfHx8fHw%3D" alt="" /></div>
+        <div className="details" id='tm7'><div>Men Lace up Sneaker Shoes</div>
+          <div className='gridd'>
+            <div id='am7'>Rs.1450</div>
+            <div ><button className='cartbtn' onClick={btnclick} value='cm7' data-text="tm7" data-amount="am7">Add to cart</button></div>
+            <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
+          </div></div>
+        <div className="details" id='tm8'>Men Leather Sneaker Shoes<div></div>
+          <div className='gridd'>
+            <div id='am8'>Rs.2559</div>
+            <div ><button className='cartbtn' onClick={btnclick} value='cm8' data-text="tm8" data-amount="am8">Add to cart</button></div>
+            <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
+          </div></div>
+          <div className="card flexbox" id='cm9'><img src="https://images.unsplash.com/photo-1512755051947-dea0029e93ad?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8MTE1MTA0MTh8fGVufDB8fHx8fA%3D%3D" alt="" /></div>
+        <div className="card flexbox" id='cm10'><img src="https://images.unsplash.com/photo-1527016021513-b09758b777bd?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8OTM2MTIxMXx8ZW58MHx8fHx8" alt="" /></div>
+        <div className="details" id='tm9'>
+          <div>Mens Trendy Overcoat Jacket</div>
+          <div className='gridd'>
+            <div id='am9'>Rs.3280</div>
+            <div ><button className='cartbtn' onClick={btnclick} value='cm9' data-text="tm9" data-amount="am9">Add to cart</button>
             </div>
             <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
           </div>
         </div>
-        <div className="details">
-        <div>Cream Embroidered Polo Slim Fit Shirt</div>
+        <div className="details" id='tm10'>
+        <div>Mens Trendy Denim Shirt</div>
           <div className='gridd'>
-            <div>Rs.450</div>
-            <div >Add to cart</div>
+            <div id='am10'>Rs.1380</div>
+            <div ><button className='cartbtn' onClick={btnclick} value='cm10' data-text="tm10" data-amount="am10">Add to cart</button></div>
             <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
           </div>
         </div>
-        <div className="card flexbox" id='cm1'><img src="https://images.unsplash.com/photo-1581068505339-d155712f0add?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8bkt3OXFTSGUtcHd8fGVufDB8fHx8fA%3D%3D" alt="" /></div>
-        <div className="card flexbox" id='cm2'><img src="https://images.unsplash.com/photo-1619466122087-e1ff06cf234b?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8d2pLcklEVzc5RUV8fGVufDB8fHx8fA%3D%3D" alt="" /></div>
-        <div className="details">
-          <div>Cream Embroidered Polo Slim Fit Shirt</div>
+        <div className="card flexbox" id='cm11'><img src="https://images.unsplash.com/photo-1581068505339-d155712f0add?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8bkt3OXFTSGUtcHd8fGVufDB8fHx8fA%3D%3D" alt="" /></div>
+        <div className="card flexbox" id='cm12'><img src="https://images.unsplash.com/photo-1619466122087-e1ff06cf234b?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8d2pLcklEVzc5RUV8fGVufDB8fHx8fA%3D%3D" alt="" /></div>
+        <div className="details" id='tm11'>
+          <div>Mens Stylish Street Shoes</div>
           <div className='gridd'>
-            <div>Rs.450</div>
-            <div >Add to cart
+            <div id='am11'>Rs.1899</div>
+            <div ><button className='cartbtn' onClick={btnclick} value='cm11' data-text="tm11" data-amount="am11">Add to cart</button>
             </div>
             <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
           </div>
         </div>
-        <div className="details">
-        <div>Cream Embroidered Polo Slim Fit Shirt</div>
+        <div className="details" id='tm12'>
+        <div>Mens Stylish Street Shoes</div>
           <div className='gridd'>
-            <div>Rs.450</div>
-            <div >Add to cart</div>
+            <div id='am12'>Rs.1899</div>
+            <div ><button className='cartbtn' onClick={btnclick} value='cm12' data-text="tm12" data-amount="am12">Add to cart</button></div>
             <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
           </div>
         </div>
-        <div className="card flexbox" id='cm1'><img src="https://images.unsplash.com/photo-1496637721836-f46d116e6d34?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8OTAyNTU2Mjl8fGVufDB8fHx8fA%3D%3D" alt="" /></div>
-        <div className="card flexbox" id='cm2'><img src="https://images.unsplash.com/photo-1563891217861-7924b471afb3?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8MTY3Nzc3N3x8ZW58MHx8fHx8" alt="" /></div>
-        <div className="details">
-          <div>Cream Embroidered Polo Slim Fit Shirt</div>
+        <div className="card flexbox" id='cm13'><img src="https://images.unsplash.com/photo-1496637721836-f46d116e6d34?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8OTAyNTU2Mjl8fGVufDB8fHx8fA%3D%3D" alt="" /></div>
+        <div className="card flexbox" id='cm14'><img src="https://images.unsplash.com/photo-1563891217861-7924b471afb3?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8MTY3Nzc3N3x8ZW58MHx8fHx8" alt="" /></div>
+        <div className="details" id='tm13'>
+          <div>100% UV protected Wayfarer Sunglasses</div>
           <div className='gridd'>
-            <div>Rs.450</div>
-            <div >Add to cart
+            <div id='am13'>Rs.750</div>
+            <div ><button className='cartbtn' onClick={btnclick} value='cm13' data-text="tm13" data-amount="am13">Add to cart</button>
             </div>
             <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
           </div>
         </div>
-        <div className="details">
-        <div>Cream Embroidered Polo Slim Fit Shirt</div>
+        <div className="details" id='tm14'>
+        <div>UV protected Trendy Sunglasses</div>
           <div className='gridd'>
-            <div>Rs.450</div>
-            <div >Add to cart</div>
+            <div id='am14'>Rs.450</div>
+            <div ><button className='cartbtn' onClick={btnclick} value='cm14' data-text="tm14" data-amount="am14">Add to cart</button></div>
             <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
           </div>
         </div>
         
-        <div className="card flexbox" id='cm1'><img src="https://images.unsplash.com/photo-1586882829491-b81178aa622e?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8MTE3MjE3NzZ8fGVufDB8fHx8fA%3D%3D" alt="" /></div>
-        <div className="card flexbox" id='cm2'><img src="https://images.unsplash.com/photo-1588099768550-4014589e03e0?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8OTk4Mzk3MHx8ZW58MHx8fHx8" alt="" /></div>
-        <div className="details">
-          <div>Cream Embroidered Polo Slim Fit Shirt</div>
+        <div className="card flexbox" id='cm15'><img src="https://images.unsplash.com/photo-1586882829491-b81178aa622e?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8MTE3MjE3NzZ8fGVufDB8fHx8fA%3D%3D" alt="" /></div>
+        <div className="card flexbox" id='cm16'><img src="https://images.unsplash.com/photo-1588099768550-4014589e03e0?w=294&dpr=2&h=294&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8OTk4Mzk3MHx8ZW58MHx8fHx8" alt="" /></div>
+        <div className="details" id='tm15'>
+          <div>Men's Magnum casual Sneakers</div>
           <div className='gridd'>
-            <div>Rs.450</div>
-            <div >Add to cart
+            <div id='am15'>Rs.3750</div>
+            <div ><button className='cartbtn' onClick={btnclick} value='cm15' data-text="tm15" data-amount="am15">Add to cart</button>
             </div>
             <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
           </div>
         </div>
-        <div className="details">
-        <div>Cream Embroidered Polo Slim Fit Shirt</div>
+        <div className="details" id='tm16'>
+        <div>Men suede Sneaker shoes</div>
           <div className='gridd'>
-            <div>Rs.450</div>
-            <div >Add to cart</div>
+            <div id='am16'>Rs.3450</div>
+            <div ><button className='cartbtn' onClick={btnclick} value='cm16' data-text="tm16" data-amount="am16">Add to cart</button></div>
             <div >4.7<img src="https://cdn-icons-png.flaticon.com/512/541/541415.png" alt="" /></div>
           </div>
         </div>
